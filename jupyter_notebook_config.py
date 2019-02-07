eac46a202ce89cb689a4fbce42c7cca867b41f1c
Carlos Giraldo <cgiraldo@gradiant.org>
2019-02-07 09:36:05 +0100

    added jupyter configuration

---
+++ jupyter_notebook_config.py
@@ -0,0 +1,9 @@
+## The directory to use for notebooks and kernels.
+c.NotebookApp.notebook_dir = '/notebooks'
+## Token used for authenticating first-time connections to the server.
+#
+#  When no password is enabled, the default is to generate a new, random token.
+#
+#  Setting to an empty string disables authentication altogether, which is NOT
+#  RECOMMENDED.
+c.NotebookApp.token = ''
